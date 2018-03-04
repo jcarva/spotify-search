9314d83652f1cda72a35ecc0a1615980c24c8b18
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2018-03-04 05:20:19 -0500

    VP #5: Create root components

---
+++ src/root/root.component.js
@@ -0,0 +1,16 @@
+// @flow
+
+// Dependencies
+import * as React from 'react'
+
+// Components
+import Routes from './root.routes'
+
+// Main Component
+type Props = {
+  store: Object
+}
+
+const Root = ({store}: Props) => <Routes />
+
+export default Root
