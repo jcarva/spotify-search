87b06fb3acceb1c84d103153da72e85fbf9f1562
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2018-03-04 03:29:20 -0500

    VP #12: Refactor initial code base to follow new code-style rules

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 // @flow
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 type Props = {};
 
 type State = {};
 
 class App extends Component<Props, State> {
-
   handleClassClick = (event: Object): void => console.log(event.target.className)
 
-  render() {
+  render () {
     return (
-      <div className="App" onClick={this.handleClassClick}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to VPlayer</h1>
+      <div className='App' onClick={this.handleClassClick}>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to VPlayer</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
