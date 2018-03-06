f5c5344937ce2264c0220d6a1631baa5905afaaf
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2018-03-05 21:12:56 -0500

    VP #6: Update routes general layout

--- src/root/root.routes.js
+++ src/root/root.routes.js
@@ -10,23 +10,14 @@ const NoMatch = () => <h1>NoMatch</h1>
 
 // Main Component
 export default () => (
-  <div id='root'>
-    <div>
-      <h1>Header</h1>
-      {window.location.pathname.includes('index.html') && <Redirect to='/' />}
-    </div>
-    <div>
-      <h1>SideBar</h1>
-      <Router onUpdate={() => window.scrollTo(0, 0)}>
-        <Switch>
-          <Redirect exact from='/' to='/home' />
-          <Route path='/home' component={Home} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
-    </div>
-    <div>
-      <h1>FooterBar</h1>
-    </div>
+  <div id='routes'>
+    {window.location.pathname.includes('index.html') && <Redirect to='/' />}
+    <Router onUpdate={() => window.scrollTo(0, 0)}>
+      <Switch>
+        <Redirect exact from='/' to='/home' />
+        <Route path='/home' component={Home} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
   </div>
 )
