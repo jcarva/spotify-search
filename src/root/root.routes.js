8cd4ab4310789b33e1cdb5df97d2b82b0df3ed47
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2018-03-05 22:54:28 -0500

    VP #6: Create album and artist pages
    Update routes

--- src/root/root.routes.js
+++ src/root/root.routes.js
@@ -7,6 +7,8 @@ import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom
 // Pages
 import Login from '../pages/login/login.container'
 import Artists from '../pages/artists/artists.container'
+import Artist from '../pages/artist/artist.container'
+import Album from '../pages/album/album.container'
 
 // Main Component
 export default () => (
@@ -17,6 +19,8 @@ export default () => (
         <Redirect exact from='/' to='/login' />
         <Route path='/login' component={Login} />
         <Route path='/home' component={Artists} />
+        <Route path='/artist' component={Artist} />
+        <Route path='/album' component={Album} />
         <Redirect to='/home' />
       </Switch>
     </Router>
