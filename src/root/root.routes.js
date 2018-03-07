512e4cc5504226f0785256706e465bd7320a664f
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2018-03-07 14:07:12 -0500

    VP #16: Create PrivateRoute component
    Update routes workflow

--- src/root/root.routes.js
+++ src/root/root.routes.js
@@ -4,11 +4,19 @@
 import * as React from 'react'
 import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
 
+// Services
+import spotifyApi from '../services/spotify'
+
 // Pages
 import Login from '../pages/login/login.container'
 import Artists from '../pages/artists/artists.container'
 import Artist from '../pages/artist/artist.container'
 import Album from '../pages/album/album.container'
+
+// Private Route
+const PrivateRoute = (props) => spotifyApi.getAccessToken()
+  ? <Route {...props} />
+  : <Redirect to='/login' />
 
 // Main Component
 export default () => (
@@ -18,9 +26,10 @@ export default () => (
       <Switch>
         <Redirect exact from='/' to='/login' />
         <Route path='/login' component={Login} />
-        <Route path='/home' component={Artists} />
-        <Route path='/artist' component={Artist} />
-        <Route path='/album' component={Album} />
+        <PrivateRoute path='/home' component={Artists} />
+        <PrivateRoute path='/artists' component={Artists} />
+        <PrivateRoute path='/artist' component={Artist} />
+        <PrivateRoute path='/album' component={Album} />
         <Redirect to='/home' />
       </Switch>
     </Router>
