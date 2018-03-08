310bf14eaca70244b5c6214954c411d4c709788d
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2018-03-08 14:19:21 -0500

    VP #18: Update artists page to fetch its needed data

--- src/pages/artists/artists.container.js
+++ src/pages/artists/artists.container.js
@@ -3,24 +3,47 @@
 // Dependencies
 import * as React from 'react'
 
+// Services
+import spotifyApi from '../../services/spotify'
+
 // Assets
 import './artists.scss'
 
 // Interfaces
+type Props = {
+  history: Object
+};
+
 type State = {
-  nowPlaying: {
-    name: string,
-    albumArt: string
-  }
+  artists: Array<Object>
 };
 
 // Main Component
-class Artists extends React.Component<void, State> {
-  constructor (context: any) {
+class Artists extends React.Component<Props, State> {
+  constructor (props: Props, context: any) {
     super(context)
     this.state = {
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
+      artists: []
     }
+  }
+
+  /**
+   * Stores a new group of artists in the component's state
+   * @param  {Array<Object>} artists The group of artists that will be stored
+   * @return {Void}
+   */
+  setArtists = (artists: Array<Object>) => this.setState({artists})
+
+  /**
+   * Fetchs the top user artists when the component will mount to update the inital state
+   * @return {Void}
+   */
+  componentWillMount = () => {
+    spotifyApi.getMyTopArtists()
+      .then(
+        (data) => this.setArtists(data.items),
+        (err) => console.error(err)
+      )
   }
 
   render () {
@@ -28,7 +51,15 @@ class Artists extends React.Component<void, State> {
       <div id='artists-page'>
       Artists Page
         <div>
-          Now Playing: { this.state.nowPlaying.name }
+          {
+            this.state.artists.map((artist) => {
+              return (
+                <div key={artist.id} className='artist-tile' onClick={() => this.props.history.push(`/artists/${artist.id}`)}>
+                  {artist.name}
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     )
