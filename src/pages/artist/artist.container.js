56c518be98254f2dd945784655b94b9028192e3d
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2018-03-08 14:29:24 -0500

    VP #18: Update artist page to fetch its needed data

--- src/pages/artist/artist.container.js
+++ src/pages/artist/artist.container.js
@@ -3,32 +3,85 @@
 // Dependencies
 import * as React from 'react'
 
+// Services
+import spotifyApi from '../../services/spotify'
+
 // Assets
 import './artist.scss'
 
 // Interfaces
+type Props = {
+  history: Object,
+  match: Object
+}
+
 type State = {
-  nowPlaying: {
-    name: string,
-    albumArt: string
-  }
+  artist: Object,
+  albums: Array<Object>
 };
 
 // Main Component
-class Artist extends React.Component<void, State> {
-  constructor (context: any) {
+class Artist extends React.Component<Props, State> {
+  constructor (props: Props, context: any) {
     super(context)
     this.state = {
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
+      artist: {
+        id: 'Loading',
+        name: 'Loading'
+      },
+      albums: []
     }
+  }
+
+  /**
+   * Stores an artist in the component's state
+   * @param  {Object} artist The artist that will be stored
+   * @return {Void}
+   */
+  setArtist = (artist: Object) => this.setState({artist})
+
+  /**
+   * Stores a group of albums in the component's state
+   * @param  {Array<Object>} albums The group of albums that will be stored
+   * @return {Void}
+   */
+  setAlbums= (albums: Array<Object>) => this.setState({albums})
+
+  /**
+   * Fetchs the current artists and its albums to update the inital component's state
+   * @return {Void}
+   */
+  componentWillMount = () => {
+    spotifyApi.getArtist(this.props.match.params.id)
+      .then(
+        (data) => this.setArtist(data),
+        (err) => console.error(err)
+      )
+
+    spotifyApi.getArtistAlbums(this.props.match.params.id)
+      .then(
+        (data) => this.setAlbums(data.items),
+        (err) => console.error(err)
+      )
   }
 
   render () {
     return (
       <div id='artist-page'>
       Artist Page
+        <div className='artist-tile'>
+          {this.state.artist.name}
+        </div>
         <div>
-          Now Playing: { this.state.nowPlaying.name }
+          {
+            this.state.albums.map((album) => {
+              return (
+                <div key={album.id} className='album-tile' onClick={() => this.props.history.push(`/albums/${album.id}`)}>
+                  {album.name}
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     )
